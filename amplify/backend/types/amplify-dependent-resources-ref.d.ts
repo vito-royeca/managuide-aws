f094ea6807e9c2e06f7713ea2bbd9ad5b0781981
Vito Royeca <vito.royeca@gmail.com>
2022-07-05 22:44:24 -0400

    Added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "managuideawse56d02b2": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
